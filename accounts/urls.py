ed5556a856346741f204b8204e27ba033180e354
Seyfullah Demirci <sdemirci55@gmail.com>
2018-10-30 13:12:16 -0700

    google auth added

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,5 @@
 from django.urls import path
+from django.conf.urls import url, include
 
 from . import views
 
@@ -6,5 +7,6 @@ urlpatterns = [
     path('login', views.login, name='login'),
     path('register', views.register, name='register'),
     path('logout', views.logout, name='logout'),
+    url('', include('social_django.urls', namespace='social')),
     path('dashboard', views.dashboard, name='dashboard')
 ]
